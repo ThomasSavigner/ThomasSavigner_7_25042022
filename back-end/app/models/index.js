fcf61e14479713df461e482f0657f50479701434
thomthom-dev <tsavigner@outlook.com>
2022-05-01 22:52:39 +0200

    app connected to remote mysql database

---
+++ back-end/app/models/index.js
@@ -0,0 +1,31 @@
+const dbConfig = require("../config/db.config");
+const Sequelize = require("sequelize");
+
+// connection
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: 0,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+// test database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  })
+;
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+//db.user = require("./user.model.js")(sequelize, Sequelize);
+module.exports = db;
--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const db = require("./app/models");
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 app.use(cors(corsOptions));
+
 // parse requests of content-type - application/json
 app.use(express.json());
+
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+// synchronization  
+db.sequelize.sync();
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Hello World !!!" });
@@ -18,3 +26,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
