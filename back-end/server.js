4d87a094d12c7df3abb3e3fb3c468a9493faea56
thomthom-dev <tsavigner@outlook.com>
2022-05-08 20:33:17 +0200

    delete data code after  insert in roles and dpts tables

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const db = require("./app/models");
-const controller = require("./app/controllers/user.controller");
+
 
 // use of CORS
 var corsOptions = {
@@ -17,45 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const run = async () => {
-  
-  // departments creation in db
-  const dpt1 = await controller.createDepartment({
-    name: "Administration",
-  });
-  const dpt2 = await controller.createDepartment({
-    name: "Finances",
-  });
-  const dpt3 = await controller.createDepartment({
-    name: "Comptabilité",
-  });
-  const dpt4 = await controller.createDepartment({
-    name: "SSI",
-  });
-  const dpt5 = await controller.createDepartment({
-    name: "Marketing",
-  });
-  const dpt6 = await controller.createDepartment({
-    name: "Logistique",
-  });
-
-  // roles creation in db
-  const role1 = await controller.createRole({
-    userRole: "user",
-  });
-  const role2 = await controller.createRole({
-    userRole: "admin",
-  });
-  const role3 = await controller.createRole({
-    userRole: "root",
-  });
-}
-
 // connection & synchronization app models with database
 db.sequelize.sync({ force: true })
   .then( () => { 
     console.log("Synchronization models/database complete");
-  run();
   }
 );
 
